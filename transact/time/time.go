2b727b2ef3dda454e9a7a29e8e3e18489b9ab88c
Sithum Bopitiya <sithumsandeepap@gmail.com>
2021-08-25 18:03:46 +0530

    Add time and change color

--- transact/time/time.go
+++ transact/time/time.go
@@ -14,17 +14,18 @@ var mainTime *time.Time
 
 func (obj *Time) GetTime() Time {
 	if mainTime == nil {
-		pointTime := time.Now()
+		pointTime := time.Now().Add(time.Second * 60)
 		mainTime = &pointTime
 	}
 	currentTime := time.Now()
-	difference := currentTime.Sub(*mainTime)
-	d := fmt.Sprintf("%v:%v:%v", 0-int(difference.Hours()), 0-int(difference.Minutes()), 60-int(difference.Seconds()))
-	c := "black"
-	if int(difference.Seconds()) >= 50 {
+	difference := mainTime.Sub(currentTime)
+
+	d := fmt.Sprintf("%v:%v:%v", int(difference.Hours()), int(difference.Minutes()), int(difference.Seconds()))
+	c := "white"
+	if int(difference.Seconds()) <= 10 {
 		c = "red"
 	}
-	if int(difference.Seconds()) >= 60 {
+	if int(difference.Seconds()) <= 0 {
 		d = "0:0:0"
 	}
 	return Time{
