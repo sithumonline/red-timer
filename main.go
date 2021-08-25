4dc6709ff575a798432b0b3ad48add247eafaac8
Sithum Bopitiya <sithumsandeepap@gmail.com>
2021-08-25 18:17:00 +0530

    Rm cmd exec

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "github.com/sithumonline/red-timer/cmd"
+import (
+	"github.com/sithumonline/red-timer/cmd/server"
+)
 
 func main() {
-	cmd.Execute()
+	//cmd.Execute()
+	server.Run()
 }
